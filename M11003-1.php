3b7c307e0e1262b4f63249802ab3bab4346e4742
Nicolas SWIGON <swigon.nicolas@gmail.com>
2022-04-11 15:06:16 +0200

    correction Nicolas

--- M11003-1.php
+++ M11003-1.php
@@ -2,22 +2,41 @@
 
 /* Exercice 1 */
     // Créer une variable avec le chiffre 12 
+    $number = 12;
     
     // Lui ajouter 9 via l'opérateur arithmétique correspondant
+    $number += 9;
 
     // Afficher la variable avec l'opération effectuée
-    
+    echo $number . "<br>";
     
 /* Exercice 2 */
     // Prenons l'exemple d'une somme d'argent de 450 euros, composées en billets de 5, 10, et 20 euros
+    $totalArgent = 450;
+    define("BANKNOTE_5", 5);
+    define("BANKNOTE_10", 10);
+    define("BANKNOTE_20", 20);
 
     // Nous savons que nous possédons 6 billets de 20 euros et 52 billets de 5 euros, combien restent-ils de billets de 10 euros ?
-    // Poser les opérations nécesaires au calcul ci-dessous
+
+    $argent10 = $totalArgent - (6 * BANKNOTE_20 + 52 * BANKNOTE_5);
+
+    echo $argent10 . "<br>";  
+    $argent10 /= BANKNOTE_10;
+    echo $argent10 . "<br>";  
+
+    // Poser les opérations nécessaires au calcul ci-dessous
 
 
 /* Exercice 3*/
     // La rentrée des classes approchent et les élèves de 6e sont au nombre de 155, il faut tous les placer dans des classes de 30 maximum
+    $student6 = 155;
 
     // Combien de classes aurons nous au final ? Développer ci-dessous
+    $classe = $student6 / 30;
+    echo $classe . "<br>";
 
     // Combien restera t'il d'enfants dans la dernière classe à être composée ? Développer ci-dessous
+
+    $lastStudent = $student6 % 30;
+    echo $lastStudent . "<br>";
--- M11003-2.php
+++ M11003-2.php
@@ -3,12 +3,18 @@
 /* EXERCICE 1 */
     // Après avoir déclarer une nouvelle variable ayant pour valeur 27
 
+    $number = 27;
+
     // Comparer cette variable en vérifiant si sa valeur est supérieure ou égale à 35 et dumper le résultat
 
+    var_dump ($number >= 35);
 
 /* EXERCICE 2*/
     // Après avoir défini une variable comme étant égale à 34
-
+    $var1 = 34;
     // Déclarer une autre variable ayant pour valeur "34" (au format string)
-
+    $var2 = "34";
     // Effectuer ensuite les comparaisons strictes et non strictes de ces deux variables et dumper le résultat
+    var_dump($var1 == $var2);
+    var_dump($var1 === $var2);
+    
